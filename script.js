150a4b97cf4b58fb08f2420bd64b08527843e52f
Tanveer Hussain <maliktanveerhussain459@gmail.com>
2024-08-28 09:21:18 -0700

    Started working on Portfolio

---
+++ script.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".text", {
+  strings: [
+    "Software Engineer",
+    "Full Stack Developer",
+    "MERN Stack Developer",
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  backDelay: 1000,
+  loop: true,
+});
