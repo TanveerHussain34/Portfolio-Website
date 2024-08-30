d547e9ca16b70c39fc2e4b244be7976158d80dc1
Tanveer Hussain <maliktanveerhussain459@gmail.com>
2024-08-30 11:14:06 -0700

    Home, About and Services Section Complete

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-var typed = new Typed(".text", {
+var typed = new Typed(".home-role", {
+  strings: [
+    "Software Engineer",
+    "Full Stack Developer",
+    "MERN Stack Developer",
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  backDelay: 1000,
+  loop: true,
+});
+
+var typed = new Typed(".about-role", {
   strings: [
     "Software Engineer",
     "Full Stack Developer",
