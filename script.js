a760586ea308ae1d6f635a6c78557c24f222c86a
Tanveer Hussain <maliktanveerhussain459@gmail.com>
2024-09-11 07:14:06 -0700

    Significant progress in Portfolio

--- script.js
+++ script.js
@@ -42,3 +42,23 @@ window.onscroll = () => {
     }
   });
 };
+
+const scrollToTopBtn = document.getElementById("top");
+
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+scrollToTopBtn.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
