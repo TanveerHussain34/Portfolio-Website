801f32385c55269d33c5e853b987843b3f796cfb
Tanveer Hussain <maliktanveerhussain459@gmail.com>
2024-09-21 00:40:31 -0700

    Update Portfolio Website

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Portfolio-Website
+
 This is my personal portfolio website created by using HTML, CSS and JS.
--- script.js
+++ script.js
@@ -62,3 +62,11 @@ scrollToTopBtn.onclick = function () {
     behavior: "smooth",
   });
 };
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
