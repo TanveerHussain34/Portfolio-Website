245caff58ecf204b1e321592058a71be6d69d975
Tanveer Hussain <maliktanveerhussain459@gmail.com>
2024-09-23 06:03:36 -0700

    Update a part of CSS to achieve responsiveness

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ scrollToTopBtn.onclick = function () {
 };
 
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "60%";
 }
 
 function closeNav() {
