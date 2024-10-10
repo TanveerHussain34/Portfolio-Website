0fae6ee731b46ee08ff4b5b2392201cdd73fa46b
Tanveer Hussain <maliktanveerhussain459@gmail.com>
2024-10-10 13:36:44 +0500

    added all

---
+++ script.js
@@ -0,0 +1,77 @@
+var typed = new Typed(".home-role", {
+  strings: [
+    "Software Engineer",
+    "Full Stack Developer",
+    "MERN Stack Developer",
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  backDelay: 1000,
+  loop: true,
+});
+
+var typed = new Typed(".about-role", {
+  strings: [
+    "Software Engineer",
+    "Full Stack Developer",
+    "MERN Stack Developer",
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  backDelay: 1000,
+  loop: true,
+});
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
+
+const scrollToTopBtn = document.getElementById("top");
+
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+scrollToTopBtn.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+function openNav() {
+  const screenWidth = window.innerWidth; // Get the current screen width
+
+  if (screenWidth > 480 && screenWidth <= 768) {
+    document.getElementById("mySidenav").style.width = "35%";
+  } else {
+    document.getElementById("mySidenav").style.width = "100%";
+  }
+}
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
